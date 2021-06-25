b5300d567d63cf5f952480457f46d34171bc114f
ClassicVantage <61285209+ClassicVantage@users.noreply.github.com>
2021-06-25 08:46:07 +0530

    Update WedoAnalyse.py

--- WedoAnalyse.py
+++ WedoAnalyse.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 df=pd.read_csv("data.csv")
 student_df=df.loc[df['student_id']=="TRL_zet"]
 print(df.groupby("level")["attempt"].mean())
-fig=go.Figure(go.Bar(x=student_df.groupby("level")["attempt"].mean(),y=['level 1','level 2','level 3','level 4'],
+fig=go.Figure(go.scatter(x=student_df.groupby("level")["attempt"].mean(),y=['level 1','level 2','level 3','level 4'],
 orientation='h'))
 fig.show()
 
